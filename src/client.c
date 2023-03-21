5714de4f990900b951aa510670cacbd670996b47
mauriciopereira74 <mauriciovianense@hotmail.com>
2023-03-21 19:15:43 +0000

    news

---
+++ src/client.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+#include "../includes/client.h"
+
+
+int main(int argc, char *argv[]) {
+
+    /*
+    Exemplo de input:
+    ./tracer execute -u "prog-a arg-1 (...) arg-n"
+    */
+
+   
+
+}
